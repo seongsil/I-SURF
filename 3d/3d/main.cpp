0f245129b8a6f578943f95952ad7c86e0aba277d
Seongsil Heo <huhss9981@naver.com>
2016-08-04 20:49:31 +0900

    d

--- 3d/3d/main.cpp
+++ 3d/3d/main.cpp
@@ -1,76 +1,32 @@
 #include <opencv2\highgui.hpp>
 #include <opencv2\imgproc.hpp>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 
 using namespace cv;
+using namespace std;
 
 //Global variables
-Mat src, src_gray;
-int thresh = 200;
-int max_thresh = 255;
+Mat src, src_gray, src_blur,src_threshold,src_contours;
 
-char* source_window = "Source image";
-char* corners_window = "Corners detected";
 
-//Function header
-void cornerHarris_demo(int, void*);
-
-int main()
+void main()
 {
-	//Load source image and convert it to gray
-	src = imread("Stickman.png", 1);
-	cvtColor(src, src_gray, CV_BGR2GRAY);
-	
-	namedWindow(source_window, CV_WINDOW_AUTOSIZE);
-	createTrackbar("Threshold: ", source_window, &thresh, max_thresh, cornerHarris_demo);
-	imshow(source_window, src);
-	
-	cornerHarris_demo(0, 0);
+	src = imread("hand3.jpg", 1); //이미지를 행렬화해서 불러옴
+	imshow("original image", src); //행렬화된 데이터를 이미지로 보여줌
+	waitKey(0); //아무키나 입력하기 전에 창이 닫히지 않게하는 함수
 
+	cvtColor(src, src_gray, CV_BGR2GRAY); //bgr이미지를 gray scale로 변환
+	imshow("gray image", src_gray);//이미지를 gray로 바꿔 보여줌
 	waitKey(0);
-	/* IplImage* image;
-	image = cvLoadImage("Stickman.png", 1);
 
-	cvNamedWindow("Stickman", 1);
-	cvShowImage("Stickman", image);
+	//잡음을 줄이기 위한 블러처리
+	bilateralFilter(src_gray, src_blur, 9, 75, 75, 4);
+	imshow("blur image", src_blur);
+	waitKey(0);
 
-	cvWaitKey(0);
-	cvDestroyWindow("Stickman");*/
-	return 0;
-}
+	//이미지 이진화
+	threshold(src_blur, src_threshold, 245, 255, THRESH_BINARY_INV);
+	imshow("threshold image", src_threshold);
+	waitKey(0);
 
-void cornerHarris_demo(int, void*)
-{
-
-	Mat dst, dst_norm, dst_norm_scaled;
-	dst = Mat::zeros(src.size(), CV_32FC1);
-
-	/// Detector parameters
-	int blockSize = 2;
-	int apertureSize = 3;
-	double k = 0.04;
-
-	/// Detecting corners
-	cornerHarris(src_gray, dst, blockSize, apertureSize, k, BORDER_DEFAULT);
-
-	/// Normalizing
-	normalize(dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
-	convertScaleAbs(dst_norm, dst_norm_scaled);
-
-	/// Drawing a circle around corners
-	for (int j = 0; j < dst_norm.rows; j++)
-	{
-		for (int i = 0; i < dst_norm.cols; i++)
-		{
-			if ((int)dst_norm.at<float>(j, i) > thresh)
-			{
-				circle(dst_norm_scaled, Point(i, j), 5, Scalar(0), 2, 8, 0);
-			}
-		}
-	}
-	/// Showing the result
-	namedWindow(corners_window, CV_WINDOW_AUTOSIZE);
-	imshow(corners_window, dst_norm_scaled);
 }
